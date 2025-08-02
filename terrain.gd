e66d8233f87ad361b7f0190a8f9bf48cd27390cb
Filip Jagieła <filip.jagiela@tylko.com>
2025-08-02 13:26:23 +0200

    Revert commented prints

--- terrain.gd
+++ terrain.gd
@@ -46,10 +46,10 @@ func update_terrain() -> void:
 
 			if shadow[coords] > 2 and water[coords] < 2:
 				water_change[coords] += 0.5
-				#print("Shadow at ", coords, " is high: ", shadow[coords], " increasing water to ", water_change[coords])
+				print("Shadow at ", coords, " is high: ", shadow[coords], " increasing water to ", water_change[coords])
 			if shadow[coords] < 1 and water[coords] > 0:
 				water_change[coords] -= 0.5
-				#print("Shadow at ", coords, " is low: ", shadow[coords], " decreasing water to ", water_change[coords])
+				print("Shadow at ", coords, " is low: ", shadow[coords], " decreasing water to ", water_change[coords])
 				
 			if water[coords] > 1:
 				for neighbor in get_surrounding_cells(coords):
@@ -69,7 +69,7 @@ func update_terrain() -> void:
 		for y in range(map_size.size.y):
 			var coords = Vector2i(x, y)
 			if water_change.has(coords) and water_change[coords] != 0:
-				#print("Applying water change at ", coords, ": ", water_change[coords])
+				print("Applying water change at ", coords, ": ", water_change[coords])
 				water[coords] += water_change[coords]
 				if water[coords] < 0:
 					water[coords] = 0
