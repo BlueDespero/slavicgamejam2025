8139e4113c339d0cea49395c2d400885db78295d
Daniel Jurczak <danieljur4@gmail.com>
2025-08-02 13:30:28 +0200

    Add plants buttons

--- GUI/bottom_bar.gd
+++ GUI/bottom_bar.gd
@@ -1,11 +1,30 @@
 extends HBoxContainer
-@export var plant_button: PackedScene
+const plant_button = preload("res://GUI/plant_button.tscn")
+const spritesheet = preload("res://plants/plant_assets/Crop_Spritesheet.png")
+
+func create_identifier_buttons(spritesheet: Texture2D) -> Array:
+	var identifier_positions = [
+		{"x": 0, "y": 0, "name": "Turnip"},
+		{"x": 6, "y": 0, "name": "Rose"},  
+		{"x": 0, "y": 1, "name": "Cucumber"},
+		{"x": 6, "y": 1, "name": "Tulip"}   
+	]
+	
+	var buttons = []
+	
+	for pos in identifier_positions:
+		var button = plant_button.instantiate()
+		button.initialize_from_grid(spritesheet, pos.x, pos.y, 16, pos.name)
+		buttons.append(button)
+	
+	return buttons
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for i in 10:
-		var NewButton = plant_button.instantiate()
-		add_child(NewButton)
+	var identifier_buttons = create_identifier_buttons(spritesheet)
+	for button in identifier_buttons:
+		add_child(button)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- GUI/plant_button.gd
+++ GUI/plant_button.gd
@@ -1,11 +1,31 @@
 extends Button
 
+var button_icon: Texture2D
+var button_name: String
 
-# Called when the node enters the scene tree for the first time.
+# Initialize with spritesheet grid position
+func initialize_from_grid(spritesheet: Texture2D, grid_x: int, grid_y: int, cell_size: int, name: String) -> void:
+	var x = grid_x * cell_size
+	var y = grid_y * cell_size
+
+	var atlas_texture = AtlasTexture.new()
+	atlas_texture.atlas = spritesheet
+	atlas_texture.region = Rect2(x, y, cell_size, cell_size)
+
+	button_icon = atlas_texture
+	button_name = name
+
+	text = button_name
+	icon = button_icon
+	
+	icon_alignment = HORIZONTAL_ALIGNMENT_CENTER
+	vertical_icon_alignment = VERTICAL_ALIGNMENT_TOP
+	expand_icon = true
+	
+	add_theme_constant_override("icon_h_separation", 0)
+
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
