081d37750e70b4cf2e79ef77497ee3ee703a9a65
Filip Jagieła <filip.jagiela@tylko.com>
2025-08-03 09:37:09 +0200

    Add interactions between plants

--- map/map.gd
+++ map/map.gd
@@ -10,8 +10,7 @@ func _ready():
 	player.tile_map = tile_map
 	EventBus.plant_selected.connect(Callable(self, "_on_plant_selected"))
 	EventBus.harvest_crops.connect(_on_harvest_crops)
-	
-	
+
 func _on_plant_selected(plant_cls):
 	var curr_tile = tile_map.local_to_map(player.position)
 	if planted_plants.get(curr_tile):
@@ -22,6 +21,37 @@ func _on_plant_selected(plant_cls):
 	var plant = plant_inst.create_scene(curr_tile, tile_map)
 	planted_plants[curr_tile] = plant
 	add_child(plant)
+	apply_plant_interactions(curr_tile, plant)
+
+
+func apply_plant_interactions(tile: Vector2i, this_plant: Plant):
+	var interactions = Constants.plant_interactions[this_plant.plant_name]
+
+	var surrounding_cells = tile_map.get_surrounding_cells(tile)
+	for cell in surrounding_cells:
+		var other_plant = planted_plants.get(cell)
+		if not other_plant:
+			continue
+
+		# my interaction with them
+		var interaction_value = interactions.get(other_plant.plant_name, 0)
+		if interaction_value == 0:
+			continue
+		other_plant.growth_rate += interaction_value * 0.01
+		if other_plant.growth_rate < 0:
+			other_plant.growth_rate = 0
+		elif other_plant.growth_rate > 1:
+			other_plant.growth_rate = 1
+			
+		# their interaction with me
+		var my_interaction_value = Constants.plant_interactions[other_plant.plant_name].get(this_plant.plant_name, 0)
+		if my_interaction_value == 0:
+			continue
+		this_plant.growth_rate += my_interaction_value * 0.01
+		if this_plant.growth_rate < 0:
+			this_plant.growth_rate = 0
+		elif this_plant.growth_rate > 1:
+			this_plant.growth_rate = 1
 
 func _on_harvest_crops(tile_position: Vector2i):
 	var plant = planted_plants.get(tile_position)
